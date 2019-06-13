406080d3e1b738d2fa109c63e6f32b7dd5f24442
retart1337 <48867150+retart1337@users.noreply.github.com>
2019-06-13 21:46:35 +0300

    Update buffer.py
    Fixed bugs, made name your buffer possible

--- buffer.py
+++ buffer.py
@@ -1,6 +1,7 @@
 import pyperclip
 import tkinter as tk
 from PIL import ImageGrab
+from tkinter import simpledialog
 import imagebuff
 root = tk.Tk()
 dct = {}
@@ -11,7 +12,8 @@ crntimage = 0
 root.geometry('100x100')
 def callback(e):
     tocopy = []
-    f = open(filepath+dct[e]+'.txt', 'r')
+    f = open(filepath+str(e)+'.txt', 'r')
+    print(filepath+str(e)+'.txt')
     for line in f:
         tocopy.append(line)
     pyperclip.copy(''.join(tocopy))
@@ -28,25 +30,30 @@ def create_dct():
     global crnt
     crnt+=1
     pasted = pyperclip.paste()
-    f = open(filepath+'Buffer '+str(crnt)+'.txt', 'w')
+    n = simpledialog.askstring('Input', 'Name your buffer')
+    f = open(filepath+n+'.txt', 'w')
     f.write(pasted)
-    dct.update({crnt : 'Buffer '+str(crnt)})
+    dct.update({n : crnt})
     for k,v in dct.items():
         b = tk.Button(root, text=k, command=lambda e=k:callback(e))
         b.pack()
         btnlist.append(b)
     f.close()
 def create_image():
-    global crntimage
-    crntimage+=1
-    im = ImageGrab.grabclipboard()
-    im.save(filepath+'Image'+' '+str(crntimage)+'.png','PNG')
-    dct.update({crntimage: 'Image '+str(crntimage)})
-    clear()
-    for k,v in dct.items():
-        b = tk.Button(root, text=k, command=lambda e=k:writeimage(e))
-        b.pack()
-        btnlist.append(b)
+    try:
+        global crntimage
+        crntimage+=1
+        n = simpledialog.askstring('Input', 'Name your buffer')
+        im = ImageGrab.grabclipboard()
+        im.save(filepath+n+'.png','PNG')
+        dct.update({n: crntimage})
+        clear()
+        for k,v in dct.items():
+            b = tk.Button(root, text=k, command=lambda e=k:writeimage(e))
+            b.pack()
+            btnlist.append(b)
+    except:
+        pass
 tk.Button(text='New', command = create_dct).place(x=0,y=0)
 tk.Button(text='Img', command = create_image).place(x=0,y=26)
 root.mainloop()
